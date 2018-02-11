10be3ca1944dfe2ccb02b6ac69494bb822c245fe
redmaze <36124005+redmaze@users.noreply.github.com>
2018-02-11 19:53:31 +0700

    Add files via upload
    minor change

--- shipsPlacement.py
+++ shipsPlacement.py
@@ -1,5 +1,7 @@
 import random
 
+map_size = 14
+
 def createConfShips(size):
     # meng-generate posisi kapal berdasarkan list yang tersedia
     confLarge = [["6 4 East", "12 6 north", "5 8 north", "3 2 East", "0 13 East"],["1 2 East","4 7 East", "0 10 East", "12 10 north", "10 2 north"],["11 8 north", "4 5 East", "1 10 north", "8 6 East", "5 0 East"],["12 8 north", "9 4 north", "5 1 East", "0 1 north", "1 7 East"], ["5 12 east", "12 0 north", "3 7 north", "1 1 East", "9 8 north"]]
@@ -21,5 +23,19 @@ def createConfShips(size):
     retConf.append("Submarine "+ useConf[4])
 
     return retConf
+
+def place_ships():
+    # Please place your ships in the following format <Shipname> <x> <y> <direction>
+    # Ship names: Battleship, Cruiser, Carrier, Destroyer, Submarine
+    # Directions: north east south west
+
+    ships = createConfShips(map_size)
+
+    with open(os.path.join(output_path, place_ship_file), 'w') as f_out:
+        for ship in ships:
+            f_out.write(ship)
+            f_out.write('\n')
+    return
+
 
 print (createConfShips(14))
